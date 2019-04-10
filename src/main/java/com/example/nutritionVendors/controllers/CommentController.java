e675620bf4ffefcd4ae85cd8f656969c5546cc80
heycot <nguyentu15061996@gmail.com>
2019-04-10 17:38:21 +0700

    add authorization to add new comment api

--- src/main/java/com/example/nutritionVendors/controllers/CommentController.java
+++ src/main/java/com/example/nutritionVendors/controllers/CommentController.java
@@ -10,10 +10,7 @@ import com.example.nutritionVendors.services.UserService;
 import jdk.nashorn.internal.runtime.regexp.joni.exception.InternalException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.sql.Timestamp;
 import java.util.Date;
@@ -34,40 +31,43 @@ public class CommentController {
 
 
     @PostMapping("/add")
-    public ResponseEntity addNewComment(@RequestBody CommentDTO commentDTO) throws InternalError {
+    public ResponseEntity addNewComment(@RequestHeader(value = "Authorization") String authorizationHeader, @RequestBody CommentDTO commentDTO) throws InternalError {
         try {
-            Comment comment = new Comment();
 
-            Date date= new Date();
+            if (authorizationHeader == null || authorizationHeader == "") {
 
-            long time = date.getTime();
-            System.out.println("Time in Milliseconds: " + time);
+                throw  new InternalException("Wrong authorization ");
 
-            Timestamp ts = new Timestamp(time);
+            } else {
+                User user = userService.findByToken(authorizationHeader);
 
-            User user = userService.getOneById(commentDTO.getUser_id());
-            if (user == null) {
-                throw new InterruptedException("do not have that user");
+                Comment comment = new Comment();
+
+                Date date= new Date();
+                long time = date.getTime();
+                Timestamp ts = new Timestamp(time);
+
+                ShopItem shopItem = shopItemService.getOne(commentDTO.getShopitem_id());
+                if (shopItem == null ){
+                    throw  new InternalException("do not have that shopitem");
+                }
+
+                comment.setShopItem(shopItem);
+                comment.setUser(user);
+                comment.setId(0);
+                comment.setContent(commentDTO.getContent());
+                comment.setTitle(commentDTO.getTitle());
+                comment.setCreate_date(ts);
+                comment.setRating(commentDTO.getRating());
+                comment.setStatus(1);
+
+                Integer number_comment = shopItem.getComments().size();
+                Double rating = (shopItem.getRating() * number_comment + comment.getRating() ) / (number_comment + 1);
+                shopItem.setRating(rating);
+
+                return ResponseEntity.ok(commentService.addOne(comment));
             }
 
-            ShopItem shopItem = shopItemService.getOne(commentDTO.getShopitem_id());
-            if (shopItem == null ){
-                throw  new InternalException("do not have that shopitem");
-            }
-            comment.setShopItem(shopItem);
-            comment.setUser(user);
-            comment.setId(0);
-            comment.setContent(commentDTO.getContent());
-            comment.setTitle(commentDTO.getTitle());
-            comment.setCreate_date(ts);
-            comment.setRating(commentDTO.getRating());
-            comment.setStatus(1);
-
-            Integer number_comment = shopItem.getComments().size();
-            Double rating = (shopItem.getRating() * number_comment + comment.getRating() ) / (number_comment + 1);
-            shopItem.setRating(rating);
-
-            return ResponseEntity.ok(commentService.addOne(comment));
         } catch (Exception e) {
             System.out.println(e.getCause());
             throw  new InternalException("internal exception error");
